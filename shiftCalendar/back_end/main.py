c2a9ab5f661d85fb3c171895ba8eeff651c3d8ac
Hsiao Man Lin <irene891019@gmail.com>
2025-06-30 20:06:31 -0500

    simple change

--- shiftCalendar/back_end/main.py
+++ shiftCalendar/back_end/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-from .routes import schedule, upload
+from routes import schedule, upload
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 app.include_router(upload.router)
 app.include_router(schedule.router)
+
+import os
+print("🧪 Current working dir:", os.getcwd())
+print("📁 Available files:", os.listdir("."))
+
 
 @app.get("/", include_in_schema=False)
 def redirect_to_docs():
